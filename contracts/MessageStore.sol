b118f832275a6df959d873fdd78d4d7746d309b1
SamAlpha1 <jelvehsaz@gmail.com>
2026-09-03 08:01:41 +0330

    Add upgradeable message contract

---
+++ contracts/MessageStore.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract MessageStore is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    string private message;
+
+    event MessageUpdated(string newMessage);
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(string memory initialMessage) public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+        message = initialMessage;
+    }
+
+    function setMessage(string calldata newMessage) external onlyOwner {
+        message = newMessage;
+        emit MessageUpdated(newMessage);
+    }
+
+    function getMessage() external view returns (string memory) {
+        return message;
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+}
