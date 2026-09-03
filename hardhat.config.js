3b014c7b756d096921b4f4f60579f8072de27866
SamAlpha1 <jelvehsaz@gmail.com>
2026-09-03 08:01:31 +0330

    Configure Swisstronik network

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [];
+
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: { enabled: true, runs: 200 }
+    }
+  },
+  networks: {
+    swisstronik: {
+      url: process.env.SWTR_RPC_URL || "https://json-rpc.testnet.swisstronik.com/",
+      chainId: Number(process.env.SWTR_CHAIN_ID || 1291),
+      accounts
+    }
+  }
+};
